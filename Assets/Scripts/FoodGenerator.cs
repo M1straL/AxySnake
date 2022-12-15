e26214bf5d0c42887ace611d2611eb2e10d55e4f
Eugene Pronin <e.pronin@nakusi.games>
2022-12-15 12:32:19 +0300

    Initial commit

---
+++ Assets/Scripts/FoodGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodGenerator : MonoBehaviour
+{
+    private float _time = 20f;
+    private float _timer;
+
+    [SerializeField] 
+    private GameObject _gameObject;
+    private Food _food;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = _time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timer > 0)
+        {
+            _timer -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            Instantiate(_gameObject, transform.position, Quaternion.identity);
+            _timer = _time;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour, IMoveable
+{
+    private CharacterController controller;
+    private readonly float _playerSpeed = 8.0f;
+    private readonly float _rotationSpeed = 50.0f;
+    private Camera _playerCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controller = gameObject.AddComponent<CharacterController>();
+        _playerCamera = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    public void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput,0, verticalInput);
+        movementDirection.Normalize();
+        transform.Translate(movementDirection * (_playerSpeed * Time.deltaTime), Space.World);
+    }
+
+    private void Rotate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        var eulerAngles = transform.rotation.eulerAngles;
+
+        Vector3 rotateTo = new Vector3(0, horizontalInput, 0) * (_rotationSpeed * Time.deltaTime);
+       // Vector3 rotateFrom = transform.rotation
+        
+        //transform.Rotate(eulerAngles.x, eulerAngles.y);
+        transform.Rotate(rotateTo);
+    }
+}
